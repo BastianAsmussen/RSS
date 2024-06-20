096aa3164bb08cec30ba93b7fc9216d9d59df7cb
Jacob Jørgensen <me@jacob-j.com>
2024-06-20 10:09:03 +0200

    Removed unused styles.

--- frontend/website/src/routes/+page.svelte
+++ frontend/website/src/routes/+page.svelte
@@ -16,17 +16,3 @@
 		</Badge>
 	</Card>
 </div>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-</style>
