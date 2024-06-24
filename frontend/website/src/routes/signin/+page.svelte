cbbc52a436c35843e42c5c7ed7a510a982268b84
Jacob Jørgensen <me@jacob-j.com>
2024-06-24 09:33:07 +0200

    ran prettier

--- frontend/website/src/routes/signin/+page.svelte
+++ frontend/website/src/routes/signin/+page.svelte
@@ -14,7 +14,7 @@
 	<Card>
 		<form class="flex flex-col space-y-6" on:submit={handleLogin}>
 			<h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in</h3>
-      
+
 			<FloatingLabelInput
 				style="outlined"
 				id="floating_outlined"
@@ -35,14 +35,11 @@
 
 			<div class="flex items-start">
 				<Checkbox>Remember me</Checkbox>
-				<a
-					href="/"
-					class="ms-auto text-sm text-primary-700 hover:underline dark:text-primary-500"
-				>
+				<a href="/" class="ms-auto text-sm text-primary-700 hover:underline dark:text-primary-500">
 					Forgot password?
 				</a>
 			</div>
-      
+
 			<Button type="submit" class="w-full">Login</Button>
 			<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
 				Not registered? <a
