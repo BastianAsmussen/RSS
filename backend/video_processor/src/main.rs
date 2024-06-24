640530b6e0402283d4e1c7d9108cd07d56cf6091
Bastian Asmussen <bastian@asmussen.tech>
2024-06-24 14:55:58 +0200

    feat: integrate gstreamer-video for enhanced video processing capabilities
    Added gstreamer-video dependency and updated video processing logic to utilize gstreamer for video scaling and encoding. This enhances the video processing capabilities and allows for more flexible video transformations.

--- backend/video_processor/src/main.rs
+++ backend/video_processor/src/main.rs
@@ -2,6 +2,10 @@ mod video;
 
 use anyhow::Result;
 use clap::Parser;
+use gstreamer::{
+    prelude::{ElementExt, GstObjectExt},
+    ClockTime, MessageView, State,
+};
 
 /// Video processor for RSS.
 #[derive(Parser, Debug)]
@@ -16,7 +20,25 @@ fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
     gstreamer::init()?;
 
-    video::temp()?;
+    let pipeline = video::convert("", (640, 480))?;
+    let bus = pipeline.bus().expect("Failed to get bus!");
+    pipeline.set_state(State::Playing)?;
+
+    for msg in bus.iter_timed(ClockTime::NONE) {
+        match msg.view() {
+            MessageView::Eos(..) => break,
+            MessageView::Error(err) => {
+                println!(
+                    "Error from {:?}: {} ({:?})",
+                    err.src().map(GstObjectExt::path_string),
+                    err.error(),
+                    err.debug()
+                );
+                break;
+            }
+            _ => (),
+        }
+    }
 
     Ok(())
 }
--- backend/video_processor/src/video.rs
+++ backend/video_processor/src/video.rs
@@ -1,21 +1,14 @@
-use anyhow::Result;
-use gstreamer::{Caps, ElementFactory, Pipeline};
+use anyhow::{anyhow, Result};
+use gstreamer::{glib::object::Cast, Pipeline};
 
-pub fn convert(url: &str, new_dims: (u32, u32)) -> Result<()> {
-    /* Command:
-        * gst-launch-1.0 -e uridecodebin uri=https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm name=decode ! \
-    videoscale ! 'video/x-raw,width=360,height=240' ! \
-    x264enc ! queue ! mp4mux name=mp4mux ! filesink location=0000.mp4 \
-    decode. ! audioconvert ! lamemp3enc bitrate=128 ! queue ! mp4mux.
-         */
-    let pipeline = Pipeline::default();
+pub fn convert(url: &str, new_dims: (u32, u32)) -> Result<Pipeline> {
+    let (width, height) = new_dims;
+    let pipeline = gstreamer::parse::launch(&format!(
+        "uridecodebin uri={url} ! videoscale ! video/x-raw,width={width},height={height} ! matroskamux ! filesink location={width}x{height}.mkv",
+    ))?;
+    let pipeline = pipeline
+        .dynamic_cast::<Pipeline>()
+        .map_err(|_| anyhow!("Failed to dynamically cast pipeline!"))?;
 
-    let decoder = ElementFactory::make("uridecodebin")
-        .property("uri", url)
-        .build()?;
-
-    // let video_caps = VideoCapsBuilder::new().width(1920).height(1080).build();
-    let scaler = ElementFactory::make("videoscale").build()?;
-
-    Ok(())
+    Ok(pipeline)
 }
