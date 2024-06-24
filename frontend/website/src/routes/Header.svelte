4c7d0318d7217fdc636bb0cff728978e756a8ede
Jacob Jørgensen <me@jacob-j.com>
2024-06-24 09:13:08 +0200

    Added simple Login update on header.

--- frontend/website/src/routes/Header.svelte
+++ frontend/website/src/routes/Header.svelte
@@ -1,142 +1,83 @@
-<script>
-	import { page } from '$app/stores';
+<script lang="ts">
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Button,
+		Search,
+		Avatar,
+		Dropdown,
+		DropdownItem,
+		DropdownHeader,
+		DropdownDivider,
+		DarkMode
+	} from 'flowbite-svelte';
+	import { SearchOutline } from 'flowbite-svelte-icons';
 	import logo from '$lib/images/svelte-logo.svg';
-	import github from '$lib/images/github.svg';
-	import { Search, Button, Dropdown, DropdownItem, Avatar, DropdownHeader, DropdownDivider } from 'flowbite-svelte';
+	import { signedIn } from '../store';
+	import { goto } from '$app/navigation';
+	import { onDestroy } from 'svelte';
 
+	let isSignedIn: boolean;
+
+	const unsubscribe = signedIn.subscribe(value => {
+		isSignedIn = value;
+	});
+
+	onDestroy(() => {
+		unsubscribe();
+	});
+
+	function handleSignInSignOut(): void {
+		if (isSignedIn) {
+			signedIn.set(false);
+		} else {
+			goto('/signin');
+		}
+	}
 </script>
 
-<header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
-		</a>
+<Navbar>
+	<NavBrand href="/">
+		<img src={logo} class="me-3 h-6 sm:h-9" alt="Flowbite Logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">RSS</span>
+	</NavBrand>
+	<div class="flex md:order-2">
+		<Search size="md" class="rounded-none py-2.5" placeholder="Search..." />
+
+		<Button class="rounded-s-none !p-2.5">
+			<SearchOutline class="h-6 w-6" />
+		</Button>
+
+		<NavHamburger />
+
+		<DarkMode class="border text-primary-500 dark:border-gray-800 dark:text-primary-600" />
 	</div>
+	<NavUl>
+		<NavLi href="/" active={true}>Home</NavLi>
+		<NavLi href="/about">About</NavLi>
+	</NavUl>
 
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li>
-				<Search>
-					<Button>Search</Button>
-				</Search>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class="corner">
-		<div>
-			<Avatar class="acs" src={logo} dot={{ color: 'green' }} />
-			<Dropdown triggeredBy=".acs">
-				<div slot="header" class="px-4 py-2">
-					<span class="block text-sm text-gray-900 dark:text-white">Filler Name</span>
-					<span class="block truncate text-sm font-medium">name@filler.com</span>
-				</div>
-				<DropdownItem>Dashboard</DropdownItem>
-				<DropdownItem>Settings</DropdownItem>
-				<DropdownItem slot="footer">Sign out</DropdownItem>
-			</Dropdown>
-		</div>
+	<div class="flex items-center md:order-2">
+		<Avatar id="avatar-menu" src={logo} />
 	</div>
-</header>
-
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
-	}
-
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 0.5rem;
-		color: var(--color-text);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
-		text-decoration: none;
-		transition: color 0.2s linear;
-	}
-
-	a:hover {
-		color: var(--color-theme-1);
-	}
-</style>
+	<Dropdown placement="bottom" triggeredBy="#avatar-menu">
+		{#if isSignedIn}
+			<DropdownHeader>
+				<span class="block text-sm">Filler Name</span>
+				<span class="block truncate text-sm font-medium">name@Filler.com</span>
+			</DropdownHeader>
+			<DropdownItem>Profile</DropdownItem>
+			<DropdownItem>Playlist</DropdownItem>
+		{/if}
+		<DropdownItem>History</DropdownItem>
+		<DropdownDivider />
+		<DropdownItem>Settings</DropdownItem>
+		<DropdownItem>Help</DropdownItem>
+		<DropdownItem>Send feedback</DropdownItem>
+		<DropdownDivider />
+		<DropdownItem on:click={handleSignInSignOut}>{isSignedIn ? 'Sign out' : 'Sign in'}</DropdownItem>
+	</Dropdown>
+</Navbar>
--- frontend/website/src/routes/signin/+page.svelte
+++ frontend/website/src/routes/signin/+page.svelte
@@ -1,39 +1,53 @@
-<script>
+<script lang="ts">
 	import { FloatingLabelInput, Card, Button, Checkbox } from 'flowbite-svelte';
+	import { signedIn } from '../../store';
+	import { goto } from '$app/navigation';
+
+	function handleLogin(event: Event): void {
+		event.preventDefault();
+		signedIn.set(true);
+		goto('/');  // Navigate to the home page
+	}
 </script>
 
 <div class="center">
 	<Card>
-		<form class="flex flex-col space-y-6" action="/">
+		<form class="flex flex-col space-y-6" on:submit={handleLogin}>
 			<h3 class="text-xl font-medium text-gray-900 dark:text-white">Sign in</h3>
 
-			<FloatingLabelInput style="outlined" id="floating_outlined" name="floating_outlined" type="text">
+			<FloatingLabelInput
+				style="outlined"
+				id="email"
+				name="email"
+				type="text"
+			>
 				Email
 			</FloatingLabelInput>
 
-			<FloatingLabelInput style="outlined" id="floating_outlined" name="floating_outlined" type="text">
+			<FloatingLabelInput
+				style="outlined"
+				id="password"
+				name="password"
+				type="password"
+			>
 				Password
 			</FloatingLabelInput>
 
 			<div class="flex items-start">
 				<Checkbox>Remember me</Checkbox>
-				<a href="/" class="ms-auto text-sm text-primary-700 hover:underline dark:text-primary-500"> Forgot password? </a>
+				<a href="/forgot-password" class="ms-auto text-sm text-primary-700 hover:underline dark:text-primary-500">
+					Forgot password?
+				</a>
 			</div>
 			<Button type="submit" class="w-full">Login</Button>
 			<div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-				Not registered? <a href="/signup" class="text-primary-700 hover:underline dark:text-primary-500"> Create account </a>
+				Not registered? <a
+				href="/signup"
+				class="text-primary-700 hover:underline dark:text-primary-500"
+			>
+				Create account
+			</a>
 			</div>
 		</form>
 	</Card>
 </div>
-
-<style>
-    .center {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        flex: 0.6;
-    }
-
-</style>
